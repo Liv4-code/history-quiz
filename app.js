9d155e68b9035757a78b141cce4695e0658b46a6
livi <scholtz.ice@gmail.com>
2020-11-28 18:16:50 +0200

    Created a submit event on form that compares the users answers to the correct answers and gives back your total score as a percentage.

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+const form = document.querySelector(".quiz form");
+const correctAnswers = ["B", "A", "A", "B", "A"];
 
+form.addEventListener("submit", e => {
+    e.preventDefault();
 
+    let usersAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
+
+    score = 0;
+    usersAnswers.forEach((userAnswer, index) => { 
+        if(userAnswer === correctAnswers[index]){
+            score += 20;
+        }
+    });
+
+    scrollTo(0,0);
+
+    const resultBar = document.querySelector(".result");
+    resultBar.querySelector("span").textContent = `${score}%`;
+    resultBar.style.display = "inline";
+});
 
